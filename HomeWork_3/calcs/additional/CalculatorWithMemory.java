8fb786381a32cd0a14145259fc537cab76217a8a
Maksim <Z34M@mail.ru>
2022-08-03 19:09:33 +0300

    UPD 13 task

---
+++ HomeWork_3/calcs/additional/CalculatorWithMemory.java
@@ -0,0 +1,73 @@
+package HomeWork_3.calcs.additional;
+import HomeWork_3.calcs.api.ICalculator;
+
+public class CalculatorWithMemory {
+    private final ICalculator iCalculator; //Поля
+    private long counter;
+    private double memory;
+    private double lastRes;
+    public CalculatorWithMemory(ICalculator iCalculator){ //Затем конструктор конструктор
+        this.iCalculator = iCalculator;
+    }
+
+    //Сохранение выполненного метода
+    private void lastRes(double lastResult) {
+        this.lastRes = lastResult;
+    }
+
+    public void memo() {   //Последняя опрерация в памяти
+        memory = lastRes;
+    }
+
+    public double returnMemo() { //Возвращает результат последней операции, удаляет из памяти
+        double tmp = memory;
+        memory = 0;
+        return tmp;
+    }
+
+    public long getCountOperation() {
+        return counter;
+    }
+
+    public double div(double dividend, double divider) {
+        counter++;
+        lastRes(iCalculator.div(dividend, divider));
+        return iCalculator.div(dividend, divider);
+    }
+
+    public double mult(double firstMultiply, double secondMultiply) {
+        counter++;
+        lastRes(iCalculator.mult(firstMultiply, secondMultiply));
+        return iCalculator.mult(firstMultiply, secondMultiply);
+    }
+
+    public double subtraction(double firstSubtraction, double secondSubtraction) {
+        counter++;
+        lastRes(iCalculator.subtraction(firstSubtraction, secondSubtraction));
+        return iCalculator.subtraction(firstSubtraction, secondSubtraction);
+    }
+
+    public double sum(double firstAddition, double secondAddition) {
+        counter++;
+        lastRes(iCalculator.sum(firstAddition, secondAddition));
+        return iCalculator.sum(firstAddition, secondAddition);
+    }
+
+    public double deg(double number, int numberDegree) {
+        counter++;
+        lastRes(iCalculator.deg(number, numberDegree));
+        return iCalculator.deg(number, numberDegree);
+    }
+
+    public double mod(double moduleNum) {
+        counter++;
+        lastRes(iCalculator.mod(moduleNum));
+        return iCalculator.mod(moduleNum);
+    }
+
+    public double sqNumb(double rootNum) {
+        counter++;
+        lastRes(iCalculator.sqNumb(rootNum));
+        return iCalculator.sqNumb(rootNum);
+    }
+}
---
+++ HomeWork_3/runners/CalculatorWithMemoryMain.java
@@ -0,0 +1,20 @@
+package HomeWork_3.runners;
+import HomeWork_3.calcs.additional.CalculatorWithMemory;
+import HomeWork_3.calcs.api.ICalculator;
+import HomeWork_3.calcs.simple.CalculatorWithMathCopy;
+
+public class CalculatorWithMemoryMain {
+    public static void main(String[] args){
+        ICalculator iCalculator = new CalculatorWithMathCopy();
+        CalculatorWithMemory calculatorWithMemory = new CalculatorWithMemory(iCalculator);
+        double multiplicationResult = calculatorWithMemory.mult(15.0, 7.0);
+        double divisionResult = calculatorWithMemory.div(28.0, 5.0);
+        double degreeResult = calculatorWithMemory.deg(divisionResult, 2);
+        double sumTwoResult = calculatorWithMemory.sum(multiplicationResult, degreeResult);
+        double addition = calculatorWithMemory.sum(4.1, sumTwoResult); //Считаем
+        calculatorWithMemory.memo();
+        System.out.println(addition);
+        System.out.println(calculatorWithMemory.getCountOperation());
+        System.out.println(calculatorWithMemory.returnMemo());
+    }
+}
