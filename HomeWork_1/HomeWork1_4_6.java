f541f7bea4d016c114bc3b19e8fce301024f657e
Maksim <Z34M@mail.ru>
2022-08-31 00:23:21 +0300

    UPD:  1 HomeWork. Part of homework6

--- HomeWork_1/HomeWork1_4_6.java
+++ HomeWork_1/HomeWork1_4_6.java
@@ -1,7 +1,7 @@
 package HomeWork_1;
 import java.util.Scanner;
 public class HomeWork1_4_6 {
-    public static void main() {
+    public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
         System.out.println("Введите интересующий вас год");
         int yea = console.nextInt();
